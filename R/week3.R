affb2f61dd3fcc756b1bbf6656a9f0fb1d0de375
Lindsey-R <105560190+Lindsey-R@users.noreply.github.com>
2024-01-31 16:41:22 -0600

    Finished part 2

--- R/week3.R
+++ R/week3.R
@@ -2,3 +2,8 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # Data Import and Cleaning
+raw_df <- read.csv(file = "../data/week3.csv", header = TRUE)
+raw_df$timeStart <- as.POSIXct(raw_df$timeStart, format = "%Y-%m-%d %H:%M:%S")
+raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd, format = "%Y-%m-%d %H:%M:%S")
+clean_df <- raw_df[raw_df$timeStart >= "2017-07-01 00:00:00", ]
+clean_df <- clean_df[clean_df$q6 == 1, ]

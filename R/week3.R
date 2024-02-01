0290f72a0e46dcab3006205fa6e905e7bc1a8f1e
Lindsey-R <105560190+Lindsey-R@users.noreply.github.com>
2024-02-01 12:18:18 -0600

    Changed bar plot and added comments

--- R/week3.R
+++ R/week3.R
@@ -2,7 +2,7 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # Data Import and Cleaning
-raw_df <- read.csv(file = "../data/week3.csv", header = TRUE)
+raw_df <- read.csv(file = "../data/week3.csv", header = TRUE) #use../ to go back to the psy8712-week3 directory
 raw_df$timeStart <- as.POSIXct(raw_df$timeStart, format = "%Y-%m-%d %H:%M:%S")
 raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd, format = "%Y-%m-%d %H:%M:%S")
 clean_df <- raw_df[raw_df$timeStart >= "2017-07-01 00:00:00", ]
@@ -15,4 +15,4 @@ frequency_tables_list <- lapply(clean_df[ ,5:14], table)
 lapply(frequency_tables_list, barplot)
 nrow(clean_df[clean_df$q1 >= clean_df$q2 & clean_df$q2 != clean_df$q3, ])
 for (i in 1:length(frequency_tables_list)){
-  barplot(unlist(frequency_tables_list[i]))}
+  barplot(unlist(frequency_tables_list[i]))} #unlist, otherwise error: non-numeric for binary args
